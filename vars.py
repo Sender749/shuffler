285afec1c2923595cdecad342217b48c63110322
Sender749 <jotmahal2003@gmail.com>
2026-01-27 16:17:00 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,16 +1,16 @@
 import os
 from typing import List
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
+API_ID = int(os.getenv("API_ID", "25208597"))
+API_HASH = os.getenv("API_HASH", "e99c3c5693d6d23a143b6ce760b7a6de")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
-DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", ""))
-ADMIN_ID = int(os.getenv("ADMIN_ID", ""))
-PICS = (os.environ.get("PICS", "")).split()
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://gd3251791:tvTkKkoJFybHhB5w@cluster0.b2a0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1003136895050"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "6541030917"))
+PICS = (os.environ.get("PICS", "s://envs.sh/iKu.jpg https://envs.sh/iKE.jpg https://envs.sh/iKe.jpg https://envs.sh/iKi.jpg https://envs.sh/iKb.jpg")).split()
 LOG_CHNL = int(os.getenv("LOG_CHNL", ""))
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "TechifyBots") # Without @
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "Navex_69") # Without @
 IS_FSUB = bool(os.environ.get("FSUB", False))
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "").split()))
-DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_LOG", ""))
+DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_LOG", "-1003137381162"))
 FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "240"))
