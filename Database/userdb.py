3724feface504d0af7627b3612d93fc10f16b78e
Sender749 <jotmahal2003@gmail.com>
2026-01-27 21:40:49 +0530

    Update userdb.py

--- Database/userdb.py
+++ Database/userdb.py
@@ -1,17 +1,3 @@
-'''
-Smile, please... 😊
-Goodbye forever.
-
-Thank you all for your care and support throughout this journey.
-
-Unfortunately, my @dypixx account has been hacked. The hacker is now using it to promote unknown content. Please be cautious, stay alert, and don’t fall for anything they post.
-
-Take care, stay safe — and once again, thank you for everything.
-Goodbye.
-
-— Dypixx
-'''
-
 from typing import Any
 from vars import MONGO_URI
 from motor import motor_asyncio
@@ -111,3 +97,4 @@ class dypixx:
             return None
 
 udb = dypixx()
+
